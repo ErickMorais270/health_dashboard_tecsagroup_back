20cf479695dc43f090670fde4b21008c0e955b71
ErickMorais270 <erickmorais270@gmail.com>
2026-05-10 04:07:01 -0300

    Refactor web routes to use HealthController for the root endpoint; added strict types declaration for improved type safety.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\Api\HealthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HealthController::class);
